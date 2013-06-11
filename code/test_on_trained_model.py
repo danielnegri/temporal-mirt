3b274abd15ea029b673856664492418189cbcb92
Jascha Sohl-Dickstein <jascha.sohldickstein@gmail.com>
2013-06-10 18:13:48 -0700

    command line arguments for prediction script

--- code/test_on_trained_model.py
+++ code/test_on_trained_model.py
@@ -1,3 +1,4 @@
+import argparse
 import accuracy_model_util as acc_util
 import sys
 from temporal_mirt import TMIRTResource
@@ -6,7 +7,7 @@ import numpy as np
 linesplit = acc_util.linesplit
 
 
-def load_data_to_test(model, indexer='plog', fname='data/test'):
+def load_data_to_test(model, indexer='plog', fname='/data/eliana/first_problem_log/test', max_users=np.inf):
     """
     Create a new TMIRT model, and load it with training data.
     """
@@ -21,6 +22,8 @@ def load_data_to_test(model, indexer='plog', fname='data/test'):
     print >>sys.stderr, "loading data"
     # loop through all the training data, and create user objects
     for line in fileinput.input(fname):
+        if model.users.num_users == max_users:
+            break
         # split on either tab or \x01 so the code works via Hive or pipe
         row = linesplit.split(line.strip())
         # the user and timestamp are shared by all row types.
@@ -44,7 +47,7 @@ def load_data_to_test(model, indexer='plog', fname='data/test'):
             row[idx_pl.time_taken] = float(row[idx_pl.time_taken])
         resources.append(TMIRTResource(row, idx_pl))
 
-    if len(resources) > 1:
+    if len(resources) > 1 and model.users.num_users < max_users:
         # flush the data for the final user, too
         model.users.add_user(user, resources[:-1])
         last_answers.append(resources[-1])
@@ -55,18 +58,52 @@ def load_data_to_test(model, indexer='plog', fname='data/test'):
     return last_answers
 
 
-tmirt = np.load("data/tmirt_file=train_abilities=3_epoch=17.npz")
-tmirt = tmirt['tmirt'][()]
-tmirt.reset_users()
-last_answers = load_data_to_test(tmirt)
-tmirt.predict_performance(N_avg_samp=20, hmc_steps=10, hmc_burnin=100)
+def get_cmd_line_arguments():
+    parser = argparse.ArgumentParser(
+        description="Generate a CSV file with predicted and actual accuracy "
+                    "on test data, and an NPZ file holding the resulting "
+                    "model state.")
+    parser.add_argument("model", type=str, help="TMIRT model filename.")
+    parser.add_argument("test", type=str, help="Test dataset filename.")
+    parser.add_argument("out", type=str, default="test_predictions", help="Base filename for output.")
+    parser.add_argument("-m", "--max_users", type=int, default=1e4,
+        help="The maximum number of users to run prediction on.")
+    parser.add_argument("-n", "--num_samples", type=int, default=20,
+        help="The number of samples to average over for the prediction.")
+    parser.add_argument("-s", "--hmc_steps", type=int, default=10,
+        help="The number of sampling steps to take between samples.  Burnin will be 10 times this number.")
+    parser.add_argument("-L", "--LBFGS_steps", type=int, default=100,
+        help="The number of LBFGS steps to take for the MAP estimate.")
 
-f = open('roc', 'w')
-for i in range(len(last_answers)):
-    f.write(
-        str(1 if last_answers[i].correct else 0) + ' ' +
-        str(tmirt.predict_user_exercise_performance(
-            i, last_answers[i].name))+'\n')
+    # DEBUG use parse_known_args rather than parse_args so can easily run it
+    # inside pylab
+    options, _ = parser.parse_known_args()
 
-np.savez('out.npz', model=tmirt, predict=tmirt.predict_performance(),
-         last=last_answers)
+    return options
+
+
+def main():
+    options = get_cmd_line_arguments()
+
+    print options
+
+    tmirt = np.load(options.model)
+    tmirt = tmirt['tmirt'][()]
+    tmirt.reset_users()
+    last_answers = load_data_to_test(tmirt, max_users=options.max_users)
+    print "Number users %d"%(tmirt.users.num_users)
+    assert(tmirt.users.num_users == len(last_answers))
+    tmirt.predict_performance(N_avg_samp=options.num_samples, hmc_steps=options.hmc_steps, hmc_burnin=options.hmc_steps*10)
+
+    f = open(options.out + ".roc", 'w')
+    for i in range(len(last_answers)):
+        f.write(
+            str(1 if last_answers[i].correct else 0) + ' ' +
+            str(tmirt.predict_user_exercise_performance(
+                i, last_answers[i].name))+'\n')
+
+    np.savez(options.out + '.npz', model=tmirt, predict=tmirt.predict_performance(),
+             last=last_answers)
+
+if __name__ == '__main__':
+    main()
